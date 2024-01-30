2815397c41b6172902739a738e3adc206e04ba7b
AlessioZeni <alessio.zeni01@gmail.com>
2024-01-30 13:56:40 +0100

    test arduino

--- src/common/protocol/test-arduino/src/main.cpp
+++ src/common/protocol/test-arduino/src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
+#include "protocol.hpp"
+#include "hal.hpp"
+Protocol protocol;
+SerialHal hal;
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int available(void * t){
+  return Serial.available();
 }
+
+void send(void * t, unsigned char c){
+  Serial.write(c);
+}
+
+unsigned char read(void * t){
+  return Serial.read();
+}
+
+void flush(void* t){
+  Serial.flush();
+}
+
+unsigned char buffer[40];
+unsigned long time;
+void setup() {
+  Serial.begin(115200);
+  hal.init(NULL, available, send, read, flush);
+  protocol.init(hal);
+  for(int i=0; i<40; i++){
+    buffer[i]=0;
+  }
+  time=millis();
+}
+
+int count =0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(millis()-time>=14){
+    time=millis();
+    count ++;
+    sprintf((char *) buffer , "it works %d", count);
+    int len=strlen((char * )buffer);
+    //buffer[len]=0;
+    protocol.checker.send_msg(buffer, len);
+    delay(1);
+  }
+  
 }
