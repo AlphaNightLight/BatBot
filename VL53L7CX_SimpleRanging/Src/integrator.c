f25365ed63afb41bcae18a0be810fd0037b7f3b1
Stypox <stypox@pm.me>
2024-02-20 16:52:00 +0100

    Cleanup integrator code

--- VL53L7CX_SimpleRanging/Src/integrator.c
+++ VL53L7CX_SimpleRanging/Src/integrator.c
@@ -17,11 +17,11 @@ void integrator_calibrate(Integrator* this, AccelGyroData data) {
 
 int tt=0;
 void integrator_update(Integrator* this, AccelGyroData data, double deltat) {
-	this->rotation = vector_sum(this->rotation, vector_multiplied(vector_sum(data.gyro, vector_multiplied(this->calibratedGyro, -1)), deltat));
+  this->rotation = vector_sum(this->rotation, vector_multiplied(vector_sum(data.gyro, vector_multiplied(this->calibratedGyro, -1)), deltat));
 
   Vector accel = data.accel;//rotate(data.accel, rotation);
 
-  if (tt==0 || isnan(this->rotation.z) || isnan(this->rotation.y)){
+  if (tt==0){
 	  printf("dt=%.5lf", deltat);
 	  printf("  g_data="); vector_print(&data.gyro);
 	  printf("  rotation="); vector_print(&this->rotation);
@@ -30,14 +30,13 @@ void integrator_update(Integrator* this, AccelGyroData data, double deltat) {
 	  //printf("  a_old="); vector_print(&accel);
   }
 
-  double lastAccelDiff = vector_length(vector_sum(accel, vector_multiplied(this->lastAccel, -1)));
   this->lastAccel = accel;
   
   accel = vector_sum(accel, vector_multiplied(this->calibratedAccel, -1));
   this->velocity = vector_sum(this->velocity, vector_multiplied(accel, deltat));
   this->position = vector_sum(this->position, vector_multiplied(this->velocity, deltat));
 
-  if (tt==0 || isnan(this->rotation.z) || isnan(this->rotation.y)){
+  if (tt==0) {
 	  printf("  a_new="); vector_print(&accel);
 	  printf("  vel="); vector_print(&this->velocity);
 	  printf("  pos="); vector_print(&this->position);
@@ -45,6 +44,4 @@ void integrator_update(Integrator* this, AccelGyroData data, double deltat) {
     tt=100;
   }
   --tt;
-
-  if (isnan(this->rotation.z) || isnan(this->rotation.y)) while(1);
 }
